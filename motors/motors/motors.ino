9c4e38015c7d4688b271506f075009dd995997b1
jerrr36 <58893325+jerrr36@users.noreply.github.com>
2020-09-24 21:12:27 -0700

    Update motors.ino
    Updated motor controls. Need to focus on i2c connections with other Arduino next.

--- motors/motors/motors.ino
+++ motors/motors/motors.ino
@@ -1,15 +1,43 @@
-<include Wire.h>
-<include Servo.h>
+#include <Adafruit_MotorShield.h>
+#include <Wire.h>
+#include <Servo.h>
 
-void rotateServo(){
-  //ran when 
+
+Servo myServo;
+AF_DCMotor motor1(1, MOTOR_64KHZ);
+AF_DCMotor motor2(2, MOTOR_64KHZ);
+
+float receiveRequest(){
+  //
+}
+void rotateServo(angle float){
+  //Rotating servo based on supplied angle
+  //Should prob make this a nice algorithm but I will make it simple for now
+  if (angle > 90) {
+    servo.write(180);
+    delay(4000);
+  }
+  else if (angle < -90) {
+    servo.write(0);
+    delay(4000);
+  }
+  else {
+    servo.write(angle);
+    delay(2000);
+  }
 }
 void setup() {
-  // put your setup code here, to run once:
+  motor1.setSpeed(200);
+  motor2.setSpeed(200);
+  myServo.attach(9);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  
+  receiveRequest();
+  rotateServo(angle);
+  
 }
