33faa0395beeba6b5dc616712a7d9e5cb8a44c47
jerrr36 <58893325+jerrr36@users.noreply.github.com>
2020-10-13 17:28:52 -0700

    Basic working version
    Connects to and receives from hivemq broker

--- ESP8266/mqtt_ex/mqtt_ex.ino
+++ ESP8266/mqtt_ex/mqtt_ex.ino
@@ -2,29 +2,48 @@
 #include <ESP8266WiFi.h>
 
 
-String ssid = "****";
-String pwd = "*****";
-String mqtt_server = "broker.hivemq.com";
+String ssid = "";
+String pwd = "";
+const char* mqtt_server = "broker.hivemq.com";
 
-WiFiClient client;
-PubSubClient psclient(client);
-
-#define MSG_BUFFER_SIZE (50)
-char msg[MSG_BUFFER_SIZE];
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 void wifiConnect(String ssid, String pwd);
+void callback(char* topic, byte* payload, unsigned int length);
+
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   wifiConnect(ssid, pwd);
   delay(2000);
   //tcp connexction to mqttserver
-  psclient.setServer(mqtt_server, 1883);
+  client.setServer(mqtt_server, 1883);
+
+  client.setCallback(callback);
+
+  while(!client.connected()) {
+    String clientid = "JSHESP8266";
+    Serial.println("connecting to hivemq");
+    if (client.connect(clientid.c_str())) {
+      Serial.println("Connected");
+      client.publish("esp/test","Hello");
+      client.subscribe("esp/test");
+    } else {
+      Serial.print("failed =");
+      Serial.println(client.state());
+      delay(5000);
+    }
+  }
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  client.loop();
+  client.publish("esp/test","hello");
+  delay(2000);
 
 }
 
@@ -44,4 +63,16 @@ void wifiConnect(String ssid, String pwd){
   Serial.println(WiFi.SSID());              // Tell us what network we're connected to
   Serial.print("IP address:\t");
   Serial.println(WiFi.localIP());           // Send the IP address of the ESP8266 to the computer 
+}
+
+void callback(char* topic, byte* payload, unsigned int length){
+  Serial.println(topic);
+  Serial.print("Message: ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+  Serial.println("-----------------------");
+  
+  
 }
