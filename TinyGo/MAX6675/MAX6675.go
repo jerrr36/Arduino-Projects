3d002c1e5583b0ad5aab147195e34291dc12bc1d
jerrr36 <58893325+jerrr36@users.noreply.github.com>
2020-12-07 22:37:49 -0500

    Updated comments

--- TinyGo/MAX6675/MAX6675.go
+++ TinyGo/MAX6675/MAX6675.go
@@ -17,54 +17,54 @@ var errNoTC error = errors.New("No thermocouple is connected")
 
 // Device struct
 type Device struct {
-	SCK machine.Pin
-	CS  machine.Pin
-	SO  machine.Pin
+	sck machine.Pin
+	cs  machine.Pin
+	so  machine.Pin
 }
 
-// New creates a new tc struct
+// New creates a new Thermocouple device
 func New(sck machine.Pin, cs machine.Pin, so machine.Pin) Device {
 	return Device{sck, cs, so}
 
 }
 
-//Configure MAX6675
+// Configure MAX6675 sets the necessary pins as outputs
 func (d *Device) Configure() {
-	d.SCK.Configure(machine.PinConfig{Mode: machine.PinOutput})
-	d.CS.Configure(machine.PinConfig{Mode: machine.PinOutput})
-	d.SO.Configure(machine.PinConfig{Mode: machine.PinOutput})
-	d.CS.High()
+	d.sck.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	d.cs.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	d.so.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	d.cs.High()
 }
 
-//spiRead reads 8 bits from the max6675 chip. Not exported
+// spiRead reads 8 bits from the max6675 chip
 func (d *Device) spiRead() uint16 {
 	var i int
 	var b uint16 = 0
 	for i = 7; i >= 0; i-- {
-		d.SCK.Low()
+		d.sck.Low()
 		time.Sleep(time.Microsecond * 10)
-		if d.SO.Get() {
+		if d.so.Get() {
 			b |= (1 << i)
 		}
-		d.SCK.High()
+		d.sck.High()
 		time.Sleep(time.Microsecond * 10)
 	}
 
 	return b
 }
 
-//ReadTemperature Function either returns the temperature in millidegrees of celsius or an error
+// ReadTemperature Function either returns the temperature in millidegrees of celsius or an error
 func (d *Device) ReadTemperature() (uint32, error) {
 	var data uint16
 
-	d.CS.Low()
+	d.cs.Low()
 	time.Sleep(time.Microsecond * 10)
 
 	data |= d.spiRead()
 	data <<= 8
 	data |= d.spiRead()
 
-	d.CS.High()
+	d.cs.High()
 
 	if data == 0x4 {
 
